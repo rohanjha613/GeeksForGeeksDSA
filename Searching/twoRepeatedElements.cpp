1549b5f0d451a561977501724596ffefa866435b
Rohan Jha <jharohan613@gmail.com>
2021-10-02 16:50:11 +0530

    "New problems added"

---
+++ Searching/twoRepeatedElements.cpp
@@ -0,0 +1,54 @@
+// { Driver Code Starts
+//Initial template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+//User function template for C++
+
+class Solution {
+  public:
+    //Function to find two repeated elements.
+    vector<int> twoRepeated (int arr[], int N) {
+        int first=-1,second=-1;
+        for(int i=0;i<N+2;i++)
+        {
+            if(arr[arr[i]-1] < 0){
+                if(first==-1)
+                    first=abs(arr[i]);
+                else
+                    second=abs(arr[i]);
+            }
+            else
+                arr[abs(arr[i])]=-(arr[abs(arr[i])]);
+        }
+        return {first,second};
+    }
+};
+
+// { Driver Code Starts.
+
+int main()
+{
+    int t,n;
+    cin>>t;
+    
+    while(t--)
+    {
+        cin>>n;
+        
+        int a[n+2];
+        
+        for(int i=0;i<n+2;i++)
+            cin>>a[i];
+        
+        Solution obj;
+        vector<int> res;
+        res = obj.twoRepeated(a, n);
+        cout<<res[0]<<" "<<res[1]<<endl;
+    }
+    return 0;
+}
+  // } Driver Code Ends
